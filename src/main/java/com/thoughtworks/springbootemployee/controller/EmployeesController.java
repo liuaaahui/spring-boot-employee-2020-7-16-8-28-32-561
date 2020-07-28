9dc036695c49c0c1c6f09186845fb1472ba77fe5
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-28 21:00:30 +0800

    [spring-boot-employee][penny liu] feature: implement getEmployeeById method.

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -1,8 +1,8 @@
 package com.thoughtworks.springbootemployee.controller;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +14,21 @@ import java.util.List;
 public class EmployeesController {
     @GetMapping
     public List<Employee> getAllEmployees(){
+        return createNewEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable int id){
+        List<Employee> employees = createNewEmployees();
+        for (int index = 0;index<employees.size();index++){
+            if(employees.get(index).getId()==id){
+                return employees.get(index);
+            }
+        }
+        return null;
+    }
+
+    private List<Employee> createNewEmployees() {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(4,"alibaba1",20,"male",6000));
         employees.add(new Employee(11,"tengxun2",19,"female",7000));
