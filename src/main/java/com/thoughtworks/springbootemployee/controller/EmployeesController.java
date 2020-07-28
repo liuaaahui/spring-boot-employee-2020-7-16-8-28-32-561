34e05e560e7edc8ce74d87bffc0841efd9c8b770
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-28 21:09:22 +0800

    [spring-boot-employee][penny liu] feature: implement getEmployeesByPage method.

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeesController.java
@@ -1,10 +1,8 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,10 +10,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 public class EmployeesController {
-    @GetMapping
-    public List<Employee> getAllEmployees(){
-        return createNewEmployees();
-    }
+//    @GetMapping
+//    public List<Employee> getAllEmployees(){
+//        return createNewEmployees();
+//    }
 
     @GetMapping("/{id}")
     public Employee getEmployeeById(@PathVariable int id){
@@ -28,12 +26,26 @@ public class EmployeesController {
         return null;
     }
 
+    @GetMapping
+    public List<Employee> getEmployeesByPage(@RequestParam int page, @RequestParam int pageSize){
+        List<Employee> employees = createNewEmployees();
+        int beginIndex = (page-1)*pageSize;
+        int endIndex = page*pageSize-1;
+        List<Employee> displayEmployees = new ArrayList<>();
+        for (;beginIndex<=endIndex&&beginIndex<employees.size();beginIndex++){
+            displayEmployees.add(employees.get(beginIndex));
+        }
+        return displayEmployees;
+    }
+
     private List<Employee> createNewEmployees() {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee(4,"alibaba1",20,"male",6000));
         employees.add(new Employee(11,"tengxun2",19,"female",7000));
         employees.add(new Employee(6,"alibaba3",19,"male",8000));
         employees.add(new Employee(13,"alibaba4",20,"female",9000));
+        employees.add(new Employee(15,"alibaba5",19,"male",5000));
+        employees.add(new Employee(17,"alibaba6",20,"female",4000));
         return employees;
     }
 }
